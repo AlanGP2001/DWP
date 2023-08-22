641f121c4691ea68ed1890dae7fb1409aa877bb9
AlanGP-Vemasmas <alan.gp@vemasmas.mx>
2023-08-22 11:10:04 -0600

    actializaciones 2

--- Red-Social-master/README.md
+++
@@ -1,9 +0,0 @@
-# Red Social
-
-Red social realizada con el framework Spring Boot, con una BBDD HSQLDB, con hibernate, thymeleaf y Spring Security. Y pruebas realizadas con Selenium.
-
-# Autores
-
-Alejandro Barrera Sánchez
-
-Carlos Sanabria Miranda
--- Red-Social-master/sdi1-uo250707uo251893/src/main/resources/static/script/languages.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#languageDropdownMenuButton a").click(function(e) {
-		e.preventDefault(); // cancel the link behaviour 
-		var languageSelectedValue = $(this).attr("value");
-		
-		window.location.replace('?lang=' + languageSelectedValue);
-		return false;
-	});
-});
